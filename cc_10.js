37b4881d93f5dad50fd85ec4521cbf9a99b5d914
Annabelle Fait <imannaf8@gmail.com>
2025-02-23 21:46:29 -0500

    Task 3 - Created Inventory Class

--- cc_10.js
+++ cc_10.js
@@ -40,3 +40,23 @@ const order1 = new Order(501, prod1, 2); // created order 1
 
 console.log(order1.getOrderDetails()); // logs order 1 details
 console.log(prod1.getDetails()); // logs product 1 current details
+
+// Task 3 - Created Inventory Class
+
+class Inventory {
+    constructor() {
+        this.products = []
+        this.orders = [];
+    } // Created class inventory with products and orders
+    addProduct(product) {
+        this.products.push(product);
+    } // Adds a new product to the end of the array
+    listProducts() {
+        this.products.forEach(product =>{
+            console.log(product.getDetails());
+        }) // Logs the details of the products
+    }
+}
+const inventory = new Inventory(); // creates a new inventory
+inventory.addProduct(prod1); // Adds prod1 to the current inventory
+inventory.listProducts(); // Lists all of the products
